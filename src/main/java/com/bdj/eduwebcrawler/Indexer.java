9ffa39a7df1bd84b5ce7dba92c5e24682591b33c
Derek Sayler <sayler.derek@gmail.com>
2019-03-06 18:24:37 -0800

    First implementation of Lucene indexing and searching

---
+++ src/main/java/com/bdj/eduwebcrawler/Indexer.java
@@ -0,0 +1,63 @@
+package com.bdj.eduwebcrawler;
+
+import java.io.IOException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class Indexer implements AutoCloseable {
+    private IndexWriter writer;
+
+    public Indexer(String indexDir) throws IOException {
+        this(Paths.get(indexDir));
+    }
+
+    public Indexer(Path directory) throws IOException {
+        //this directory will contain the indexes. FS directory takes Path object
+        Directory indexDirectory = FSDirectory.open(directory);
+
+        //standard analyzer does analysis bases on basic grammar, removes stopwords, converts to lowercase
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+
+        //create the indexer
+        writer = new IndexWriter(indexDirectory, new IndexWriterConfig(analyzer));
+    }
+
+    public void deleteAll() throws IOException {
+        //Delete all content where IndexWriter writes
+        writer.deleteAll();
+    }
+
+    public void close() throws IOException {
+        //close IndexWriter
+        writer.close();
+    }
+
+    public void addDoc(String url, List<String> parentURLs, List<String> childURLs, String text) throws IOException {
+        //create new document
+        Document doc = new Document();
+
+        //add fields to document
+        StringField urlField = new StringField("url", url, Field.Store.YES);
+        doc.add(new TextField("text", text, Field.Store.YES));
+
+        for (String temp : parentURLs){
+            doc.add(new StringField("parentURLs", temp, Field.Store.YES));
+        }
+
+        for (String temp : childURLs){
+            doc.add(new StringField("childURLs", temp, Field.Store.YES));
+        }
+
+        //add document to index
+        writer.addDocument(doc);
+    }
+}
