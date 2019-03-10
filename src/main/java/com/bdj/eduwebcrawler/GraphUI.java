4db99d60ede5f7cfb42c1af26e05c324a8d2ed36
bjesse93 <bpara001@ucr.edu>
2019-03-10 16:19:22 -0700

    -Seed Graph

--- src/main/java/com/bdj/eduwebcrawler/GraphUI.java
+++ src/main/java/com/bdj/eduwebcrawler/GraphUI.java
@@ -3,19 +3,27 @@ import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
 import org.graphstream.ui.view.*;
 
+
+
  public class GraphUI{
  	protected boolean loop= true;
+
+ 	public static void Seed() {
+ 		Graph mainGraph = new SingleGraph("NodeClicks");
+ 		Viewer views = mainGraph.display();
+
+ 		views.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+
+ 	}
+
 	 public static void main(String [] args) {
-	 	new GraphUI();
+		 Graph graph = new SingleGraph("ViewerMap");
+		 Viewer views=graph.display();
+		 View viewOpt = views.getDefaultView();
+		 graph.addNode("sample");
+		 // when node is selected execute subgraph
+		 Seed();
 	 }
-
-	 public  GraphUI() {
-	 	Graph mainGraph= new SingleGraph("NodeClicks");
-	 	Viewer views= mainGraph.display();
-
-	 	views.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
-
-
-	 }
-
  }
+
+ //seeds and use the int (get_degree) levels
