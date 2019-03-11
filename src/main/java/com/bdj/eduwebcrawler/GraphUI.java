98f48e9de34705388d19754b95539dba22537fa5
bjesse93 <bpara001@ucr.edu>
2019-03-10 17:53:32 -0700

    -click and zoom feature added

--- src/main/java/com/bdj/eduwebcrawler/GraphUI.java
+++ src/main/java/com/bdj/eduwebcrawler/GraphUI.java
@@ -27,3 +27,18 @@ import org.graphstream.ui.view.*;
  }
 
  //seeds and use the int (get_degree) levels
+// 3 options edu web-crawler query visuals
+/*
+  public void IGraph() {
+        Graph graphnew = new SingleGraph("InputGraph");
+        Viewer Vviewers = graphnew.display();
+        Vviewers.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+        ViewerPipe fPipe = Vviewers.newViewerPipe();
+        fPipe.addViewerListener((ViewerListener) this);
+        fPipe.addSink(graphnew);
+        while (loop) {
+            fPipe.pump();
+        }
+    }
+
+ */
