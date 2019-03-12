23955113a4ec1001d1b8c7674eba5fbcc298652b
Derek Sayler <sayler.derek@gmail.com>
2019-03-12 11:06:32 -0700

    Finished implementation of Querent, and keywords.

--- src/main/java/com/bdj/eduwebcrawler/Querent.java
+++ src/main/java/com/bdj/eduwebcrawler/Querent.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Querent {
 
@@ -15,27 +16,36 @@ public class Querent {
         try{
             Searcher searcher = new Searcher("Lucene/index/");
             while(true) {
-                System.out.println("Enter a query of any length. To stop enter QUIT.");
+                System.out.println("\nEnter a query of any length. To stop enter QUIT.");
                 Scanner reader = new Scanner(System.in);  // Reading from System.in
-                String n = reader.next();
+                String n = reader.nextLine();
                 if (n.equals("QUIT")) {
                     break;
                 }
                 try {
                     ScoreDoc[] hits = searcher.queryText(n);
-                    int cnt = 0;
+                    int cnt = 1;
                     for (ScoreDoc hit: hits) {
                         Document d = searcher.getDocByDocId(hit.doc);
-                        Map<String, Integer> kws = searcher.getKeywordsByDocId(hit.doc);
-                        System.out.println("Rank: " + cnt);
-                        System.out.println("url: " + d.get("url"));
-                        System.out.println("Keywords: ");
-                        Iterator it = kws.entrySet().iterator();
-                        while (it.hasNext()) {
-                            Map.Entry pair = (Map.Entry)it.next();
-                            System.out.println("         " + pair.getKey() + " freq: " + pair.getValue());
-                            it.remove(); // avoids a ConcurrentModificationException
+                        System.out.println("\nRank: " + cnt);
+                        if(d.getField("title") != null)
+                            System.out.println("Title:\n" + d.get("title"));
+                        System.out.println("URL: " + d.get("url"));
+                        if(d.getField("description") != null)
+                            System.out.println("Description:\n" + d.get("description"));
+                        if(d.getField("keywords") != null)
+                            System.out.println("Keywords:\n" + d.get("keywords"));
+                        else {
+                            Map<String, Integer> kws = searcher.getKeywordsByDocId(hit.doc);
+                            System.out.println("Keywords: ");
+                            Iterator it = kws.entrySet().iterator();
+                            while (it.hasNext()) {
+                                Map.Entry pair = (Map.Entry)it.next();
+                                System.out.println("          " + pair.getKey() + " freq: " + pair.getValue());
+                                it.remove(); // avoids a ConcurrentModificationException
+                            }
                         }
+                        cnt++;
                     }
                 }
                 catch (ParseException e) {
